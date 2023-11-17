3210aa860411dcf0978f0e654084579dd12394eb
Melvin Mathew <71694894+melvinmathew108@users.noreply.github.com>
2023-11-17 10:34:39 +0530

    Second commit

--- sworker.js
+++ sworker.js
@@ -6,7 +6,7 @@ self.addEventListener("install", (e) => {
         ".style.css",
         "./assets/logo-small.png",
         "./assets/logo-large.png",
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+       
       ]);
     })
   );
